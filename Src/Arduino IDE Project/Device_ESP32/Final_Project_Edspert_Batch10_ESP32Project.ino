2a5ee24f7b3b9fb24a5dc5b47318fb28724620fd
Devan Cakra Mudra Wijaya <54527592+cakraawijaya@users.noreply.github.com>
2024-12-25 12:33:07 +0700

    Update: 25/12/2024

--- Src/Arduino IDE Project/Device_ESP32/Final_Project_Edspert_Batch10_ESP32Project.ino
+++ Src/Arduino IDE Project/Device_ESP32/Final_Project_Edspert_Batch10_ESP32Project.ino
@@ -181,7 +181,6 @@ void setup() {
   Serial.begin(115200); // Baudrate untuk papan ESP
   fc28.begin(); // Memulai sensor fc-28
   dht.begin(); // Memulai sensor dht
-  pinMode(PIN_LDR, INPUT); // LDR sebagai input
   pinMode(PIN_WATERPUMP, OUTPUT); // Pompa sebagai output
   connectToWiFi(); // Memanggil method connectToWiFi
 }
